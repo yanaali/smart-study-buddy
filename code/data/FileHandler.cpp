697c1fd94c05864616a80e87930e9de86a0c5a97
Aaliyan Muhammad <151714070+yanaali@users.noreply.github.com>
2026-02-22 22:54:49 -0800

    Add code/data/FileHandler.cpp
    This file is prepared for future implementations of FileHandler functions.

---
+++ code/data/FileHandler.cpp
@@ -0,0 +1,4 @@
+#include "FileHandler.h"
+
+// Definitions for FileHandler functions will be implemented in D3.
+// For Deliverable 2, the functions are inline in the header, so this file remains empty.
